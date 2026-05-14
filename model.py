1397cff9f0d3421fe5df1cdc9e59817e92618af1
Anandhakrishnan <anandhakrishnanm21@gmail.com>
2026-05-14 18:00:06 +0530

    model update -default values for vocab size

--- model.py
+++ model.py
@@ -340,8 +340,8 @@ class Transformer(nn.Module):
 
     def __init__(
         self,
-        src_vocab_size: int,
-        tgt_vocab_size: int,
+        src_vocab_size: int = 10,
+        tgt_vocab_size: int = 10,
         d_model: int = 512,
         N: int = 6,
         num_heads: int = 8,
